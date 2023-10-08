c9f2d49e3fcb02e42efc7f90e224eede371830f4
ShimanshuChauhan <shivanshuchauhan699@gmail.com>
2023-10-08 21:30:34 +0530

    added delete card functionality

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,30 @@
-async function modify() {
+function modify() {
   let html = "";
-  const data = JSON.parse(localStorage.getItem("quickMemo"));
-  for (let i = 0; i < data.length; i++) {
+  const data =
+    JSON.parse(localStorage.getItem("quickMemo"));
+
+
+  if (!data || data.length == 0) {
+    const fText = 'Welcome to QuickMemo!<br><br>Hover to learn more'
+    const bText = ''
     html += `
      <article class="card">
-        <h1 class="front">${data[i].question}</h1>
-        <p class="back">
-          ${data[i].answer}
-        </p>
+        <div class="front"><h1>${fText}</h1></div>
+        <div class="back">${bText}<p> hi </p></div>
       </article>`;
   }
-
+  else {
+    for (let i = 0; i < data.length; i++) {
+      html += `
+     <article class="card">
+        <div class = "front"><h1>${data[i].question}</h1></div>
+        <div class="back"><p>
+          ${data[i].answer}
+        </p></div>
+        <button class = "card__button" id = "delete" onclick="deleteItem(${data[i].id})"> Delete </button>
+      </article>`;
+    }
+  }
   document.getElementById("mainContainer").innerHTML = html;
 }
 
@@ -35,12 +49,13 @@ function addItem() {
   const addButton = document.getElementById("submit");
 
   addButton.addEventListener("click", function () {
+
+    if (questionAdd.value == 0 || answer.value == 0) {
+      alert("Kya kar rha hai bhai tu, value to de")
+      return;
+    }
+
     let list = [];
-    var HTMLdata = {
-      question: questionAdd.value,
-      answer: answer.value,
-    };
-    const data = localStorage.getItem("quickMemo");
     const id = localStorage.getItem("counter");
     if (id) {
       const num = parseInt(id);
@@ -48,6 +63,15 @@ function addItem() {
     } else {
       localStorage.setItem("counter", 1);
     }
+    const newId = localStorage.getItem("counter");
+    const newNum = parseInt(newId)
+
+    var HTMLdata = {
+      question: questionAdd.value,
+      answer: answer.value,
+      id: newNum
+    };
+    const data = localStorage.getItem("quickMemo");
     if (data) {
       const allData = JSON.parse(data);
       list = [...allData, HTMLdata];
@@ -56,11 +80,20 @@ function addItem() {
     }
     localStorage.setItem("quickMemo", JSON.stringify(list));
     const question = document.getElementsByClassName("questionCard")[0];
-    console.log("KINNIaddButton", question);
+
     question.classList.remove("questionCardHover");
     window.location.reload();
   });
   // Remove the class questionCardHover
+}
+
+function deleteItem(id) {
+  console.log(localStorage.getItem("quickMemo"));
+  const data = JSON.parse(localStorage.getItem("quickMemo"))
+  const newList = data.filter((d) => d.id != id);
+  localStorage.setItem("quickMemo", JSON.stringify(newList));
+
+  window.location.reload();
 }
 
 modify();
